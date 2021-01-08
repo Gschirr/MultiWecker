52371a2335236e9c0a87f50c7567094f36b9b6d8
Stefan <gs4534@mci4me.at>
2021-01-08 13:37:59 +0100

    Added onClick Eventhandler to switch to second Activity

--- app/src/main/java/com/example/multiwecker/MainActivity.java
+++ app/src/main/java/com/example/multiwecker/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.multiwecker;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //private Button newRoutineButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button newRoutineButton = findViewById(R.id.createRoutine);
+        newRoutineButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent second = new Intent(MainActivity.this, createRoutine.class);
+                startActivity(second);
+            }
+        });
     }
+
+
 }
